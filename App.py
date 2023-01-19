38a2545b206f7854fc4b7e3e120abfd98c1d7e8c
Visto Preto <evppdepf@protonmail.ch>
2023-01-19 14:35:47 -0300

    update

--- App.py
+++ App.py
@@ -1,15 +1,8 @@
+#!/usr/bin/python
+
 #coding: UTF-8
 from flask import Flask, render_template, url_for
 import os
-
-class Cache_Medias():
-	def cache_create():
-		path_videos = os.listdir("static/videos")
-		for i in path_videos:
-			os.system(r"ffmpeg -i static/videos/{} static/cache/{}.png -n".format(i, i[:-4]))
-
-		path_cache = os.listdir("static/cache")
-		return path_cache
 
 
 __author__ = "VistoPreto"
@@ -18,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def	homepage():
-	return render_template('index.html', path_cache = Cache_Medias.cache_create())
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0")
